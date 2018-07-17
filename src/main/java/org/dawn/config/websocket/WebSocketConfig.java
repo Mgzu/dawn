5293860a04bea139bf4fe3f799464705c40de746
serch <2931170625@qq.com>
2018-07-17 11:47:07 +0800

    add pulllog and fix typo

---
+++ src/main/java/org/dawn/config/websocket/WebSocketConfig.java
@@ -0,0 +1,60 @@
+package org.dawn.config.websocket;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import javax.annotation.PostConstruct;
+
+import org.dawn.log.LoggerMessage;
+import org.dawn.log.LoggerQueue;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry config) {
+		config.enableSimpleBroker("/topic");
+		config.setApplicationDestinationPrefixes("/app");
+	}
+
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/ws").withSockJS();
+	}
+
+	@Autowired
+	private SimpMessagingTemplate messagingTemplate;
+
+	/**
+	 * 推送日志到/topic/pullLogger
+	 */
+	@PostConstruct
+	public void pushLogger() {
+		ExecutorService executorService = Executors.newFixedThreadPool(2);
+		Runnable processLog = () -> {
+			while (true) {
+				try {
+					LoggerMessage log = LoggerQueue.getInstance().poll();
+					if (log != null) {
+						if (messagingTemplate != null)
+							messagingTemplate.convertAndSend("/topic/pullLogger", log);
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		};
+		// 增加队列消费能力
+		executorService.submit(processLog);
+		executorService.submit(processLog);
+	}
+
+}
